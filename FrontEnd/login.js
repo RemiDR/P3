71200afae7be8ed438fe88d33cf001ca52cb5a7d
RemiDR <remidinosaure@gmail.com>
2023-09-27 11:11:08 +0200

    Login correctif

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -4,28 +4,27 @@ console.log(submit)
 submit.addEventListener("submit", (e) => {
     e.preventDefault()
     console.log(e.target.email.value)
-    const obj={
-        email:e.target.email.value,
-        password:e.target.motDePasse.value,
+    const obj = {
+        email: e.target.email.value,
+        password: e.target.motDePasse.value,
     }
     console.log(JSON.stringify(obj))
-    fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body:   JSON.stringify(obj),
-    })
-    .then(r=>r.json())
-    .then(resp=>{
-        console.log(resp)
-        localStorage.setItem("token", resp.token)
-    })
-    if(e.target.email.value==="sophie.bluel@test.tld" &&e.target.motDePasse.value === "S0phie")
-        {
-        location.href="/FrontEnd"
-        }
-        else{
-            alert("Erreur mauvais email ou mot de passe")
-        }
+    if (e.target.email.value === "sophie.bluel@test.tld" && e.target.motDePasse.value === "S0phie") {
+        fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(obj),
+        })
+            .then(r => r.json())
+            .then(resp => {
+                console.log(resp)
+                localStorage.setItem("token", resp.token)
+                location.href = "/FrontEnd"
+            })
+    }
+    else {
+        alert("Erreur mauvais email ou mot de passe")
+    }
 })
 
 
