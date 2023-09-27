7d41c197fa87024051e9298612ea567a79ce28c1
RemiDR <remidinosaure@gmail.com>
2023-09-27 10:26:04 +0200

    Login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -4,23 +4,28 @@ console.log(submit)
 submit.addEventListener("submit", (e) => {
     e.preventDefault()
     console.log(e.target.email.value)
-    alert("ok")
     const obj={
         email:e.target.email.value,
         password:e.target.motDePasse.value,
     }
     console.log(JSON.stringify(obj))
-    fetch("https://localhost:5678/api/users/login", {
+    fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body:   JSON.stringify(obj),
     })
     .then(r=>r.json())
-    .then(resp=>{console.log(resp)})
+    .then(resp=>{
+        console.log(resp)
+        localStorage.setItem("token", resp.token)
+    })
+    if(e.target.email.value==="sophie.bluel@test.tld" &&e.target.motDePasse.value === "S0phie")
+        {
+        location.href="/FrontEnd"
+        }
+        else{
+            alert("Erreur mauvais email ou mot de passe")
+        }
 })
 
-if (email != "sophie.bluel@test.tld" || password != "S0phie")
-{
-    alert("email ou mot de passe invalide")
-}
 
